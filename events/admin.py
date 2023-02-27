8e324e9d0124777098d61c68041ade77b093fd6a
Dante-Cadiz <dantecadiz98@mail.com>
2023-02-27 16:52:30 +0000

    Register EventInstance model in admin.py

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import EventInstance
 
 # Register your models here.
+
+@admin.register(EventInstance)
+class EventInstanceAdmin(admin.ModelAdmin):
+    pass
